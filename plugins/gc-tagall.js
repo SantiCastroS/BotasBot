72159239a2890cca69e1dfbdbf2eec29997b9f58
SantiCastroS <121774006+SantiCastroS@users.noreply.github.com>
2023-01-01 18:32:37 -0500

    Add files via upload

---
+++ plugins/gc-tagall.js
@@ -0,0 +1,18 @@
+let handler = async(m, { isOwner, isAdmin, conn, text, participants, args, command }) => {
+if (!(isAdmin || isOwner)) {
+global.dfail('admin', m, conn)
+throw false
+}
+let pesan = args.join` `
+let oi = `*𝙼𝙴𝙽𝚂𝙰𝙹𝙴:* ${pesan}`
+let teks = `*⺀MENCION GENERAL⺀*\n\n❏ ${oi}\n\n❏ *𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂:*\n`
+for (let mem of participants) {
+teks += ` @${mem.id.split('@')[0]}`}
+conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
+}
+handler.help = ['tagall <mesaje>','invocar <mesaje>']
+handler.tags = ['group']
+handler.command = /^(tagall|invocar|invocacion|todos|invocación)$/i
+handler.admin = true
+handler.group = true
+export default handler
